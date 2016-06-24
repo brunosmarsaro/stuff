11cd9d0fa87ae880b761f2fa1e291819b1f8f86b
Bruno Smarsaro Bazelato <brunosmarsaro@gmail.com>
2016-06-24 18:05:58 -0300

    Update Texture.h

--- OpenGL/Texture.h
+++ OpenGL/Texture.h
@@ -1,4 +1,18 @@
-#include "../gLib.h"
+//OpenGL Libs
+#if defined __APPLE__
+    #include <OpenGL/gl.h>
+    #include <OpenGL/glu.h>
+    #include <GLUT/glut.h>
+#else
+    #include <GL/gl.h>
+    #include <GL/glu.h>
+    #include <GL/glut.h>
+#endif
+
+//General Libs
+#include <cstdlib>
+#include <iostream>
+#include <cstdio>
 
 using namespace std;
 
